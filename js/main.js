b0f6db450cd282ae32333721aa3a4790232f9760
Bunyod <84246289+Bunyood@users.noreply.github.com>
2023-01-22 20:37:11 +0500

    add carosel

--- js/main.js
+++ js/main.js
@@ -3,24 +3,25 @@ const elTabItems = document.querySelectorAll(".tab__item");
 const elTabLinks = document.querySelectorAll(".tab__link");
 var elPanelItems = document.querySelectorAll(".panel__item");
 
-elTabLinks.forEach(function(link){
+elTabLinks.forEach(function (link) {
 
-  link.addEventListener("click" , function(evt){
+  link.addEventListener("click", function (evt) {
     evt.preventDefault();
 
-    elTabItems.forEach(function(item){
+    elTabItems.forEach(function (item) {
       item.classList.remove("tab__item--active");
     })
 
     link.parentElement.classList.add("tab__item--active");
 
-    elPanelItems.forEach(function(panel){
+    elPanelItems.forEach(function (panel) {
       panel.classList.remove("panel__item--active");
     })
 
     document.querySelector(link.getAttribute("href")).classList.add("panel__item--active");
     
   })
+})
 
 $('.hero-owl-carousel').owlCarousel({
   loop: true,
@@ -103,12 +104,45 @@ $('.testimonials-owl-carousel').owlCarousel({
       items: 1,
 
 
-},
-  1223: {
-      items:1,
+    },
+    1223: {
+      items: 1,
 
 
     },
+  
   }
+})
+$('.own-creation-owl-carousel').owlCarousel({
+  loop: true,
+  margin: 32, 
+  smartSpeed:700,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
 
+    },
+    1200: {
+      items: 1.5,
+
+    },
+    
+    1467: {
+      items: 3,
+
+
+    },
+    1900: {
+      items: 3.5,
+    },
+  
+  }
 })
